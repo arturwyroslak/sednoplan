90b7b2a98a52f93635634b9a608925da6faf9493
arturwyroslak <95839215+arturwyroslak@users.noreply.github.com>
2023-08-03 21:01:36 +0200

    Update server.js

--- api/server.js
+++ api/server.js
@@ -44,7 +44,7 @@ app.post('/modals/:id', async (req, res) => {
 });
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
 app.listen(3000, () => {
